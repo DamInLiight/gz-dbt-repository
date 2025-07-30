a211e046a7836db5114899d5b02e47e2d8f5ab9e
Damien <damien.pzt+dbt@gmail.com>
2025-07-30 14:37:33 +0000

    challenge 4

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,8 @@
-select *
-from {{ref("stg_raw__facebook")}}
-union all 
-select * 
-from {{ref("stg_raw__criteo")}}
-union all
-select * 
-from {{ref("stg_raw__bing")}}
-union all 
-select * 
-from {{ref("stg_raw__adwords")}}
+{{ dbt_utils.union_relations(
+    relations = [
+        ref('stg_raw__facebook'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__adwords')
+    ],
+) }}
