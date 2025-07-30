4af263b722e21831d4df78e5294961567d9c1f98
Damien <damien.pzt+dbt@gmail.com>
2025-07-30 13:08:38 +0000

    add round to campaigns_month

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,17 +1,17 @@
 select
     extract(month from date_date) as date_month,
-    sum(nb_transactions) as nb_transactions,
-    sum(revenue) as revenue,
-    sum(avg_basket) as avg_basket,
-    sum(operational_margin) as op_margin,
-    sum(ads_margin) as ads_margin,
-    sum(ads_cost) as ads_cost,
-    sum(ads_impression) as ads_impression,
-    sum(ads_click) as ads_click,
-    sum(purchase_cost) as purchase_cost,
-    sum(margin) as margin,
-    sum(shipping_fee) as shipping_fee,
-    sum(log_cost) as log_cost
+    round(sum(nb_transactions), 2) as nb_transactions,
+    round(sum(revenue), 2) as revenue,
+    round(sum(avg_basket), 2) as avg_basket,
+    round(sum(operational_margin), 2) as op_margin,
+    round(sum(ads_margin), 2) as ads_margin,
+    round(sum(ads_cost), 2) as ads_cost,
+    round(sum(ads_impression), 2) as ads_impression,
+    round(sum(ads_click), 2) as ads_click,
+    round(sum(purchase_cost), 2) as purchase_cost,
+    round(sum(margin), 2) as margin,
+    round(sum(shipping_fee), 2) as shipping_fee,
+    round(sum(log_cost), 2) as log_cost
 from {{ ref("finance_campaigns_day") }}
 where ads_cost > 0
 group by date_month
